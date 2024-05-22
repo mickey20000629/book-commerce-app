55a944e4ef305869eca846b141e4bc054ee89e77
Mikiya Katagiri <mikiya.katagiri@gmail.com>
2024-05-23 00:45:37 +0900

    5/23

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Noto_Sans_JP } from "next/font/google";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
+import { SessionProvider } from "next-auth/react";
+import { NextAuthProvider } from "./lib/next-auth/provider";
 
 const noteSansJP = Noto_Sans_JP({ subsets: ["latin"], weight: ["400"] });
 
@@ -19,8 +21,11 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body className={noteSansJP.className}>
-        <Header />
-        {children}</body>
+        <NextAuthProvider>
+          <Header />
+          {children}
+        </NextAuthProvider>
+      </body>
     </html>
   );
 }
---
+++ app/types/types.ts
@@ -0,0 +1,11 @@
+type BookType = {
+  id: number;
+  title: string;
+  content: string;
+  price: string;
+  thumbnail: { url: string };
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type { BookType };
