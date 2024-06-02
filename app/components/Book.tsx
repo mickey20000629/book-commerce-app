0df08cde2fc2cf2c4fdf0262a104af7da8bb6373
Mikiya Katagiri <mikiya.katagiri@gmail.com>
2024-06-02 22:01:21 +0900

    コミット

--- app/components/Book.tsx
+++ app/components/Book.tsx
@@ -108,23 +108,24 @@ const Book = ({ book, isPurchased }: BookProps) => {
         </a>
 
         {showModal && (
-          <div className="absolute top-0 left-0 right-0 bottom-0 bg-slate-900 bg-opacity-50 flex justify-center items-center modal">
-            <div className="bg-white p-8 rounded-lg">
-              <h3 className="text-xl mb-4">本を購入しますか？</h3>
-              <button
-                onClick={handlePurchaseConfirm}
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4"
-              >
-                購入する
-              </button>
-              <button
-                onClick={handleCancel}
-                className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
-              >
-                キャンセル
-              </button>
-            </div>
+          <div className="fixed top-0 left-0 w-full h-full flex bg-slate-900 bg-opacity-50 items-center justify-center modal">
+          <div className="relative bg-white p-8 rounded-lg max-h-full overflow-y-auto">
+            <h3 className="text-xl mb-4">本を購入しますか？</h3>
+            <button
+              onClick={handlePurchaseConfirm}
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4"
+            >
+              購入する
+            </button>
+            <button
+              onClick={handleCancel}
+              className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
+            >
+              キャンセル
+            </button>
           </div>
+        </div>
+        
         )}
       </div>
     </>
